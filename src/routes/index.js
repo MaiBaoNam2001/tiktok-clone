49d4e681a1e9849d59c5961317878a2256887d4d
Nam Mai <maibaonam260219@gmail.com>
2023-09-27 22:47:22 +0700

    Configure Router and build Layout loading mechanism

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+// Pages
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+
+// Public routes
+const publicRoutes = [
+    { path: '/', component: Home }, // layout: undefined -> default layout
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
