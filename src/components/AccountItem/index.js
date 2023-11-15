d65948a2c004d9a6bf5de53a29334d18c54c1186
Nam Mai <maibaonam260219@gmail.com>
2023-11-15 10:58:03 +0700

    Apply PropTypes

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
@@ -26,5 +27,9 @@ function AccountItem({ data }) {
         </Link>
     );
 }
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired,
+};
 
 export default AccountItem;
--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,10 +1,11 @@
 import { useState, forwardRef } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import styles from './Image.module.scss';
 import images from '~/assets/images';
 
-function Image({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) {
+const Image = forwardRef(({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) => {
     const [fallback, setFallback] = useState('');
 
     const handleError = () => {
@@ -21,6 +22,13 @@ function Image({ src, alt, className, fallback: customFallback = images.noImage,
             onError={handleError}
         />
     );
-}
+});
 
-export default forwardRef(Image);
+Image.propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    className: PropTypes.string,
+    fallback: PropTypes.string,
+};
+
+export default Image;
