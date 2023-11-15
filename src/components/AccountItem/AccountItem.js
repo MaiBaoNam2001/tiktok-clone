197da0008457c5b97cde4817dd77f286254b48c2
Nam Mai <maibaonam260219@gmail.com>
2023-11-15 11:56:30 +0700

    Fix a problem having multiple index.js files

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+
+import styles from './AccountItem.module.scss';
+import { CheckIcon } from '../Icons/Icons';
+import Image from '../Image/Image';
+
+const cx = classNames.bind(styles);
+
+function AccountItem({ data }) {
+    return (
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.nickname} />
+
+            <div className={cx('info')}>
+                <h4 className={cx('username')}>
+                    {data.nickname}
+                    {data.tick && (
+                        <span className={cx('check')}>
+                            <CheckIcon />
+                        </span>
+                    )}
+                </h4>
+                <p className={cx('nickname')}>{data.full_name}</p>
+            </div>
+        </Link>
+    );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired,
+};
+
+export default AccountItem;
---
+++ src/layouts/components/Search/Search.js
@@ -0,0 +1,104 @@
+import { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames/bind';
+import HeadlessTippy from '@tippyjs/react/headless';
+
+import styles from './Search.module.scss';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+import { ClearIcon, LoadingIcon, SearchIcon } from '~/components/Icons';
+import { useDebounce } from '~/hooks';
+import * as searchService from '~/services/searchService';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showSearchResult, setShowSearchResult] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    const debounced = useDebounce(searchValue, 500);
+
+    const inputRef = useRef();
+
+    useEffect(() => {
+        if (!debounced.trim()) {
+            setSearchResult([]);
+            return;
+        }
+
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchService.search(debounced);
+            setSearchResult(result);
+
+            setLoading(false);
+        };
+
+        fetchApi();
+    }, [debounced]);
+
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+
+    const handleHideResult = () => {
+        setShowSearchResult(false);
+    };
+
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
+
+    return (
+        // Using a wrapper <div> tag around the reference element solves this by creating a new parentNode context.
+        <div>
+            <HeadlessTippy
+                interactive
+                visible={showSearchResult & (searchResult.length > 0)}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <div className={cx('search-account')}>Tài khoản</div>
+                            {searchResult.map((result) => (
+                                <AccountItem key={result.id} data={result} />
+                            ))}
+                        </PopperWrapper>
+                    </div>
+                )}
+                onClickOutside={handleHideResult}
+            >
+                <div className={cx('search')}>
+                    <input
+                        ref={inputRef}
+                        value={searchValue}
+                        placeholder="Tìm kiếm"
+                        spellCheck={false}
+                        onChange={handleChange}
+                        onFocus={() => setShowSearchResult(true)}
+                    />
+
+                    {!!searchValue && !loading && (
+                        <button className={cx('clear')} onClick={handleClear}>
+                            <ClearIcon />
+                        </button>
+                    )}
+
+                    {loading && <LoadingIcon className={cx('loading')} />}
+
+                    <button className={cx('search-btn')}>
+                        <SearchIcon />
+                    </button>
+                </div>
+            </HeadlessTippy>
+        </div>
+    );
+}
+
+export default Search;
